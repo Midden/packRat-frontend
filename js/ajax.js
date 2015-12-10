878afdaf7eea2ad2f4550902bdffe7027b559a0c
Kaylal12 <kaylalei224@gmail.com>
2015-12-10 10:50:39 -0500

    Fixes a href close tag thing

--- js/ajax.js
+++ js/ajax.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
 
       for (var i = 0; i < urls.length; i++) {
         console.log(urls[i].name);
-        $(".files").append("<li> File: " + "<a href=" + urls[i].url.toString() + ">" + urls[i].name.toString() + " ID: " + urls[i]._id.toString() + " Link: " + urls[i].name.toString() + " Create at: " + urls[i].createdAt +"</li>");
+        $(".files").append("<li> File: " + "<a href=" + urls[i].url.toString() + ">" + urls[i].name.toString() + "/>" + " ID: " + urls[i]._id.toString() + " Link: " + urls[i].name.toString() + " Create at: " + urls[i].createdAt +"</li>");
       }
 
 
